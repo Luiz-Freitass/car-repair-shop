8217645a85d885a6e546f59f3b7c681621e7ea2c
Luiz Carlos <luiz.freitas.juniorr@gmail.com>
2026-08-14 19:00:15 -0300

    feat: implementa operações CRUD para veículos

---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Car_Repair_Shop.Data;
+using Car_Repair_Shop.Data.Dtos.VehicleDto;
+using Car_Repair_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Repair_Shop.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VehicleController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly AppDbContext _appDbContext;
+
+    public VehicleController(IMapper mapper, AppDbContext appDbContext)
+    {
+        _mapper = mapper;
+        _appDbContext = appDbContext;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateVehicle([FromBody]CreateVehicleDto dto)
+    {
+        var clientExists = await _appDbContext.Clients.AnyAsync(c => c.Id == dto.ClientId);
+        if (!clientExists) return BadRequest($"Cliente com id {dto.ClientId} não existe.");
+
+        var vehicle = _mapper.Map<Vehicle>(dto);
+        _appDbContext.Vehicles.Add(vehicle);
+        _appDbContext.SaveChangesAsync();
+
+        await _appDbContext.Entry(vehicle).Reference(v => v.Client).LoadAsync();
+
+        var readVehicle = _mapper.Map<ReadVehicleDto>(vehicle);
+        return CreatedAtAction (nameof(GetVehicleById), new {id = vehicle.Id }, readVehicle);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllVehicles([FromQuery] int skip = 0, [FromQuery] int take = 20)
+    {
+        var vehicles = await _appDbContext.Vehicles
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync();
+        return Ok(_mapper.Map<List<ReadVehicleDto>>(vehicles));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetVehicleById(int id)
+    {
+        var vehicle = _appDbContext.Vehicles
+            .Include(v => v.Client)
+            .FirstOrDefault(v => v.Id == id);
+        if (vehicle == null) return NotFound();
+
+        return Ok(_mapper.Map<ReadVehicleDto>(vehicle));
+
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateVehicle(int id, [FromBody] UpdateVehicleDto dto)
+    {
+        var vehicle = await _appDbContext.Vehicles.FirstOrDefaultAsync(v => v.Id == id);
+        if (vehicle == null) return NotFound();
+
+        var clientExists = await _appDbContext.Vehicles.AnyAsync(c => c.Id == dto.ClientId);
+        if (!clientExists) return NotFound();
+
+        _mapper.Map(dto, vehicle);
+        await _appDbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteVehicle(int id)
+    {
+        var vehicle = await _appDbContext.Vehicles.FirstOrDefaultAsync(v => v.Id == id);
+
+        if (vehicle == null) return NotFound();
+        _appDbContext.Vehicles.Remove(vehicle);
+        await _appDbContext.SaveChangesAsync();
+        return NoContent();
+    }
+}
